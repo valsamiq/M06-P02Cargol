a79dfe464a66bb7fc745ffc347ca2e224aec7c73
Aleix Ibern <aleixmibern@gmail.com>
2017-11-27 12:55:28 +0100

    Innitial Commit, and some addings

---
+++ public_html/sintesisCargo.js
@@ -0,0 +1,29 @@
+cargolList = [
+    {name:"Osama Vin Cargolen",points:0,speed:10,dist:0},
+    {name:"Mao Tse Babosung",points:0,speed:10,dist:0},
+    {name:"Cargolf Hitgol",points:0,speed:10,dist:0},
+    {name:"Cargolito Mussolini",points:0,speed:10,dist:0},
+    {name:"Sadam Cargolesim",points:0,speed:10,dist:0},
+    {name:"Yoseph Closquelin",points:0,speed:10,dist:0}
+];
+boostCargol=[];
+ArrayBoost = [];
+
+var newRace=document.getElementById("newRace");
+var buttonNRace=document.getElementById("buttonNRace");
+
+function Cargols(){
+    this.nom;
+    this.punts;
+    this.vel;
+    this.dist;
+}
+
+var arrayRank = [];
+//De moment començarem amb aquests:
+
+
+function showCargol(){
+    var show = document.getElementById("showRank");
+    show.innerHTML+=<div>arrayCargol[0][1]</div>;
+}
